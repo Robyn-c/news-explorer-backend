8356349bfe4f1789bf0ee083ccfda7b1e6961383
Robyn Cuadra <scuadra02@gmail.com>
2023-07-19 15:47:54 -0400

    fix: add port number in app.js

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ const connectDB = async () => {
   }
 };
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
   connectDB();
 });
+
